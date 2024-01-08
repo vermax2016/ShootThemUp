60fb7d6582598c407cb1f82e65da0fc05d7efa7c
vermax2016 <vermax2016@gmail.com>
2024-01-08 18:48:11 +0300

    Line Trace. Рефакторинг.

--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -31,4 +31,9 @@ protected:
     virtual void BeginPlay() override;
 
     void MakeShot();
+    APlayerController* GetPlayerController() const;
+    bool GetPlayerViewPoint(FVector& ViewLocation, FRotator& ViewRotation) const;
+    FVector GetMuzzleWorldLocation() const;
+    bool GetTraceData(FVector& TraceStart, FVector& TraceEnd) const;
+    void MakeHit(FHitResult& HitResult, const FVector& TraceStart, const FVector& TraceEnd);
 };
