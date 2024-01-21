347509f92afb7982ab147bebc6fcf794b590bf3e
vermax2016 <vermax2016@gmail.com>
2024-01-21 17:26:15 +0300

    UserWidget для режима наблюдателя. анимация в UMG

--- Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
+++ Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
@@ -6,11 +6,7 @@
 
 float USTUPlayerHUDWidget::GetHealthPercent() const
 {
-    const auto Player = GetOwningPlayerPawn();
-    if (!Player) return 0.0f;
-
-    const auto Component = Player->GetComponentByClass(USTUHealthComponent::StaticClass());
-    const auto HealthComponent = Cast<USTUHealthComponent>(Component);
+    const auto HealthComponent = GetHealthComponent();
     if (!HealthComponent) return 0.0f;
 
     return HealthComponent->GetHealthPercent();
@@ -41,4 +37,25 @@ USTUWeaponComponent* USTUPlayerHUDWidget::GetWeaponComponent() const
     const auto Component = Player->GetComponentByClass(USTUWeaponComponent::StaticClass());
     const auto WeaponComponent = Cast<USTUWeaponComponent>(Component);
     return WeaponComponent;
+}
+
+bool USTUPlayerHUDWidget::IsPlayerAlive() const {
+    const auto HealthComponent = GetHealthComponent();
+    return HealthComponent && !HealthComponent->IsDead();
+}
+
+bool USTUPlayerHUDWidget::IsPlayerSpectating() const
+{
+    const auto Controller = GetOwningPlayer();
+    return Controller && Controller->GetStateName() == NAME_Spectating;
+}
+
+USTUHealthComponent* USTUPlayerHUDWidget::GetHealthComponent() const
+{
+    const auto Player = GetOwningPlayerPawn();
+    if (!Player) return nullptr;
+
+    const auto Component = Player->GetComponentByClass(USTUHealthComponent::StaticClass());
+    const auto HealthComponent = Cast<USTUHealthComponent>(Component);
+    return HealthComponent;
 }
--- Source/ShootThemUp/Public/UI/STUPlayerHUDWidget.h
+++ Source/ShootThemUp/Public/UI/STUPlayerHUDWidget.h
@@ -8,6 +8,7 @@
 #include "STUPlayerHUDWidget.generated.h"
 
 class USTUWeaponComponent;
+class USTUHealthComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API USTUPlayerHUDWidget : public UUserWidget
@@ -24,6 +25,13 @@ public:
     UFUNCTION(BlueprintCallable, Category = "UI")
     bool GetCurrentWeaponAmmoData(FAmmoData& AmmoData) const;
 
-    private:
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    bool IsPlayerAlive() const;
+
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    bool IsPlayerSpectating() const;
+
+private:
     USTUWeaponComponent* GetWeaponComponent() const;
+    USTUHealthComponent* GetHealthComponent() const;
 };
