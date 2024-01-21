af4b8915cc53c6882abe9f0b8d26f82e566e4fd6
vermax2016 <vermax2016@gmail.com>
2024-01-21 18:19:26 +0300

    Сборка игры. Рефакторинг.

--- Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
+++ Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
@@ -3,10 +3,11 @@
 #include "UI/STUPlayerHUDWidget.h"
 #include "Components/STUHealthComponent.h"
 #include "Components/STUWeaponComponent.h"
+#include "STUUtils.h"
 
 float USTUPlayerHUDWidget::GetHealthPercent() const
 {
-    const auto HealthComponent = GetHealthComponent();
+    const auto HealthComponent = STUUtils::GetSTUPlayerComponent<USTUHealthComponent>(GetOwningPlayerPawn());
     if (!HealthComponent) return 0.0f;
 
     return HealthComponent->GetHealthPercent();
@@ -14,7 +15,7 @@ float USTUPlayerHUDWidget::GetHealthPercent() const
 
 bool USTUPlayerHUDWidget::GetCurrentWeaponUIData(FWeaponUIData& UIData) const
 {
-    const auto WeaponComponent = GetWeaponComponent();
+    const auto WeaponComponent = STUUtils::GetSTUPlayerComponent<USTUWeaponComponent>(GetOwningPlayerPawn());
     if (!WeaponComponent) return false;
 
     return WeaponComponent->GetCurrentWeaponUIData(UIData);
@@ -22,25 +23,16 @@ bool USTUPlayerHUDWidget::GetCurrentWeaponUIData(FWeaponUIData& UIData) const
 
 bool USTUPlayerHUDWidget::GetCurrentWeaponAmmoData(FAmmoData& AmmoData) const
 {
-   
-    const auto WeaponComponent = GetWeaponComponent();
+
+    const auto WeaponComponent = STUUtils::GetSTUPlayerComponent<USTUWeaponComponent>(GetOwningPlayerPawn());
     if (!WeaponComponent) return false;
 
     return WeaponComponent->GetCurrentWeaponAmmoData(AmmoData);
 }
 
-USTUWeaponComponent* USTUPlayerHUDWidget::GetWeaponComponent() const
+bool USTUPlayerHUDWidget::IsPlayerAlive() const
 {
-    const auto Player = GetOwningPlayerPawn();
-    if (!Player) return nullptr;
-
-    const auto Component = Player->GetComponentByClass(USTUWeaponComponent::StaticClass());
-    const auto WeaponComponent = Cast<USTUWeaponComponent>(Component);
-    return WeaponComponent;
-}
-
-bool USTUPlayerHUDWidget::IsPlayerAlive() const {
-    const auto HealthComponent = GetHealthComponent();
+    const auto HealthComponent = STUUtils::GetSTUPlayerComponent<USTUHealthComponent>(GetOwningPlayerPawn());
     return HealthComponent && !HealthComponent->IsDead();
 }
 
@@ -48,14 +40,4 @@ bool USTUPlayerHUDWidget::IsPlayerSpectating() const
 {
     const auto Controller = GetOwningPlayer();
     return Controller && Controller->GetStateName() == NAME_Spectating;
-}
-
-USTUHealthComponent* USTUPlayerHUDWidget::GetHealthComponent() const
-{
-    const auto Player = GetOwningPlayerPawn();
-    if (!Player) return nullptr;
-
-    const auto Component = Player->GetComponentByClass(USTUHealthComponent::StaticClass());
-    const auto HealthComponent = Cast<USTUHealthComponent>(Component);
-    return HealthComponent;
 }
--- Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -94,7 +94,6 @@ void ASTUBaseWeapon::DecreaseAmmo()
         return;
     }
     CurrentAmmo.Bullets--;
-    LogAmmo();
 
     if (IsClipEmpty() && !IsAmmoEmpty())
     {
