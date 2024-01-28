7dd73c4f452305bdb87393ac0703bc4da4b4638d
vermax2016 <vermax2016@gmail.com>
2024-01-28 19:26:44 +0300

    Физические материалы. Impacts

--- Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
+++ Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
@@ -2,6 +2,7 @@
 
 #include "Weapon/Components/STUWeaponFXComponent.h"
 #include "NiagaraFunctionLibrary.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
 
 USTUWeaponFXComponent::USTUWeaponFXComponent()
 {
@@ -10,5 +11,16 @@ USTUWeaponFXComponent::USTUWeaponFXComponent()
 
 void USTUWeaponFXComponent::PlayImpactFX(const FHitResult& Hit)
 {
+    auto Effect = DefaultEffect;
+
+    if (Hit.PhysMaterial.IsValid())
+    {
+        const auto PhysMat = Hit.PhysMaterial.Get();
+        if (EffectsMap.Contains(PhysMat))
+        {
+            Effect = EffectsMap[PhysMat];
+        }
+    }
+
     UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), Effect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
 }
