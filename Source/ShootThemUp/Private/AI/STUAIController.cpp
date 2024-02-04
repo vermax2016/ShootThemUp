3c8943bee16d154c12a192695db2e81a653a2fc5
vermax2016 <vermax2016@gmail.com>
2024-02-04 15:33:30 +0300

    Плавное вращение AI персонажа

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -1,3 +1,17 @@
 // Shoot Them Up Game. All Rights Resrved.
 
 #include "AI/STUAIController.h"
+#include "AI/STUAICharacter.h"
+
+void ASTUAIController::OnPossess(APawn* InPawn)
+{
+    Super::OnPossess(InPawn);
+
+    const auto STUCharacter = Cast<ASTUAICharacter>(InPawn);
+    if (STUCharacter)
+    {
+        RunBehaviorTree(STUCharacter->BehaviorTreeAsset);
+    }
+
+}
+
