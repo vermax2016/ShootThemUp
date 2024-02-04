7a15218978538ef99710289f3711a9e84e1d7c85
vermax2016 <vermax2016@gmail.com>
2024-02-04 18:19:19 +0300

    Компонент восприятия UAIPerceptionComponent
    Нацеливание на игрока который находится ближе в поле видимости.

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -2,6 +2,13 @@
 
 #include "AI/STUAIController.h"
 #include "AI/STUAICharacter.h"
+#include "Components/STUAIPerceptionComponent.h"
+
+ASTUAIController::ASTUAIController()
+{
+    STUAIPerceptionComponent = CreateDefaultSubobject<USTUAIPerceptionComponent>("STUAIPerceptionComponent");
+    SetPerceptionComponent(*STUAIPerceptionComponent);
+}
 
 void ASTUAIController::OnPossess(APawn* InPawn)
 {
@@ -12,6 +19,11 @@ void ASTUAIController::OnPossess(APawn* InPawn)
     {
         RunBehaviorTree(STUCharacter->BehaviorTreeAsset);
     }
-
 }
 
+void ASTUAIController::Tick(float DeltaTime) 
+{
+    Super::Tick(DeltaTime);
+    const auto AimActor = STUAIPerceptionComponent->GetClossestEnemy();
+    SetFocus(AimActor);
+}
---
+++ Source/ShootThemUp/Public/Components/STUAIPerceptionComponent.h
@@ -0,0 +1,16 @@
+// Shoot Them Up Game. All Rights Resrved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "STUAIPerceptionComponent.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API USTUAIPerceptionComponent : public UAIPerceptionComponent
+{
+    GENERATED_BODY()
+
+public:
+    AActor* GetClossestEnemy() const;
+};
