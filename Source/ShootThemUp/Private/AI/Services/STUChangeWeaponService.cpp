11ea9a7d9e404dea4511076c664da3a5932c67df
vermax2016 <vermax2016@gmail.com>
2024-02-11 15:11:31 +0300

    Сервис смены оружия

---
+++ Source/ShootThemUp/Private/AI/Services/STUChangeWeaponService.cpp
@@ -0,0 +1,26 @@
+// Shoot Them Up Game. All Rights Resrved.
+
+#include "AI/Services/STUChangeWeaponService.h"
+#include "AIController.h"
+#include "Components/STUWeaponComponent.h"
+#include "STUUtils.h"
+
+USTUChangeWeaponService::USTUChangeWeaponService()
+{
+    NodeName = "Change Weapon";
+}
+
+void USTUChangeWeaponService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+    const auto Controller = OwnerComp.GetAIOwner();
+    if (Controller)
+    {
+        const auto WeaponComponent = STUUtils::GetSTUPlayerComponent<USTUWeaponComponent>(Controller->GetPawn());
+        if (WeaponComponent && Probability > 0 && FMath::FRand() <= Probability)
+        {
+            WeaponComponent->NextWeapon();
+        }
+    }
+
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
--- Source/ShootThemUp/Private/AI/Services/STUFindenemyService.cpp
+++ Source/ShootThemUp/Private/AI/Services/STUFindenemyService.cpp
@@ -13,6 +13,7 @@ USTUFindenemyService::USTUFindenemyService()
 
 void USTUFindenemyService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
+    UE_LOG(LogTemp, Display, TEXT("find enemy"));
     const auto Blackboard = OwnerComp.GetBlackboardComponent();
     if (Blackboard)
     {
