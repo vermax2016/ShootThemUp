602128e8aaff21f57a72219f0c806a8bcd78c73a
vermax2016 <vermax2016@gmail.com>
2024-02-13 21:06:19 +0300

    EQS. Поиск здоровья. С++ декоратор

--- Source/ShootThemUp/ShootThemUp.Build.cs
+++ Source/ShootThemUp/ShootThemUp.Build.cs
@@ -34,7 +34,9 @@ public class ShootThemUp : ModuleRules
             "ShootThemUp/Public/Weapon/Components",
             "ShootThemUp/Public/AI",
             "ShootThemUp/Public/AI/Tasks",
-            "ShootThemUp/Public/AI/Services"
+            "ShootThemUp/Public/AI/Services",
+            "ShootThemUp/Public/AI/EQS",
+            "ShootThemUp/Public/AI/Decorators"
         });
 
         // Uncomment if you are using Slate UI
