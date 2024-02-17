058acef9aac6dca4face6a81ddf4020f95b267d6
vermax2016 <vermax2016@gmail.com>
2024-02-17 11:14:23 +0300

    Спаун AI персонажей

--- Source/ShootThemUp/Public/STUCoreTypes.h
+++ Source/ShootThemUp/Public/STUCoreTypes.h
@@ -83,3 +83,12 @@ struct FImpactData
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
     FDecalData DecalData;
 };
+
+USTRUCT(BlueprintType)
+struct FGameData
+{
+    GENERATED_USTRUCT_BODY()
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game", meta = (ClampMin="1", ClampMax="100"))
+    int32 PlayersNum = 2;
+};
