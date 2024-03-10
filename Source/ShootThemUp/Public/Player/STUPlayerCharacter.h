1ce9e16b29d3be32b0cacc68fa7c9049f99a7c2e
vermax2016 <vermax2016@gmail.com>
2024-03-10 17:33:17 +0300

    Исправление артефакта камеры. Сборка игры

--- Source/ShootThemUp/Public/Player/STUPlayerCharacter.h
+++ Source/ShootThemUp/Public/Player/STUPlayerCharacter.h
@@ -8,6 +8,7 @@
 
 class UCameraComponent;
 class USpringArmComponent;
+class USphereComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUPlayerCharacter : public ASTUBaseCharacter
@@ -24,14 +25,18 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     UCameraComponent* CameraComponent;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USphereComponent* CameraCollisionComponent;
+
     virtual void OnDeath() override;
+    virtual void BeginPlay() override;
 
 public:
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
     virtual bool IsRunning() const override;
 
-    private:
+private:
     bool WansToRun = false;
     bool IsMovingForward = false;
 
@@ -41,6 +46,15 @@ public:
     void OnStartRunning();
     void OnStopRunning();
 
-    private:
     void OnStartFire();
+
+    UFUNCTION()
+    void OnCameraCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+        const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnCameraCollisionEndOverlap(
+        UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+    void CheckCameraOverlap();
 };
